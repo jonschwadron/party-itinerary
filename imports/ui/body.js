2e0dbf7f6ce7db0d55872d40bb7341dcf7a5e129
jhu <johnsonhu@gmail.com>
2016-10-29 00:15:29 -0400

    adding a server method to get AirBNB listing data

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,30 +1,40 @@
-import { Template } from 'meteor/templating';
-import { Expenses } from '../api/expenses.js';
+import {
+	Template
+}
+from 'meteor/templating';
+import {
+	Expenses
+}
+from '../api/expenses.js';
 
 import './expense.js';
 import './body.html';
 
 //show generator by default
-Session.set('generatorState',false);
-Session.set('listingInfoState',false);
+Session.set('generatorState', false);
+Session.set('listingInfoState', false);
 
 
 Template.body.helpers({
 	//meteor add session
-	showListing:  function() {
+	showListing: function() {
 		return Session.get('listingInfoState');
 	},
-	showGenerator:  function() {
+	showGenerator: function() {
 		return Session.get('generatorState');
 	},
 	expenses() {
-		return Expenses.find({}, { sort: { createdAt: 0 } });
+		return Expenses.find({}, {
+			sort: {
+				createdAt: 0
+			}
+		});
 	},
 	total() {
 		let sum = 0;
 		let cursor = Expenses.find();
 
-		cursor.forEach(function(item){
+		cursor.forEach(function(item) {
 			sum += parseInt(item.price);
 		});
 
@@ -33,13 +43,13 @@ Template.body.helpers({
 });
 
 Template.body.events({
-	'focus #from': function () {
-		$( "#from" ).datepicker();
+	'focus #from': function() {
+		$("#from").datepicker();
 	},
-	'focus #to': function () {
-		$( "#to" ).datepicker();
+	'focus #to': function() {
+		$("#to").datepicker();
 	},
-	'click #listing-toggle': function () {
+	'click #listing-toggle': function() {
 		if (Session.get('listingInfoState')) {
 			Session.set('listingInfoState', false);
 		} else {
@@ -47,7 +57,7 @@ Template.body.events({
 		}
 
 	},
-	'click #itinerary-toggle': function () {
+	'click #itinerary-toggle': function() {
 		if (Session.get('generatorState')) {
 			Session.set('generatorState', false);
 		} else {
@@ -55,7 +65,7 @@ Template.body.events({
 		}
 
 	},
-	'submit .new-listing'(event) {
+	'submit .new-listing' (event) {
 		event.preventDefault();
 
 		const target = event.target;
@@ -67,23 +77,22 @@ Template.body.events({
 		var path = a['pathname'].split('/');
 		var listingId = '';
 
-		if (path.length > 3 ){
+		if (path.length > 3) {
 			listingId = path[3];
 		} else {
 			listingId = path[2];
 		}
 
-		var clientId = '?client_id=3092nxybyb0otqw18e8nh5nty';
-		var format = '&_format=v1_legacy_for_p3'
-		var apiUrl = 'https://api.airbnb.com/v2/listings/' + listingId + clientId + format;
-		var response = HTTP.get(apiUrl, function (error, data) {
-			console.log( 'http.get ::', error, data);
+		Meteor.call('getListing', listingId, function(error, result) {
+			console.log(error);
+			console.log(result);
 		});
+
 
 		Session.set('listingInfoState', true);
 		target.listing.value = '';
 	},
-	'submit .new-expense'(event) {
+	'submit .new-expense' (event) {
 		// Prevent default browser form submit
 		event.preventDefault();
 
--- server/main.js
+++ server/main.js
@@ -1,9 +1,37 @@
 import '../imports/api/expenses.js';
 
-Meteor.startup(function () {
+Meteor.startup(function() {
 	// Listen to incoming HTTP requests, can only be used on the server
 	WebApp.connectHandlers.use(function(req, res, next) {
-	  res.setHeader("Access-Control-Allow-Origin", "*");
-	  return next();
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		return next();
 	});
 })
+
+Meteor.methods({
+	'getListing' (listingId, callback) {
+		this.unblock();
+		var clientId = '3092nxybyb0otqw18e8nh5nty';
+		var format = 'v1_legacy_for_p3';
+		// TODO need to saniize the listingId
+		var apiUrl = 'https://api.airbnb.com/v2/listings/' + listingId;
+
+		try {
+			// async call
+			HTTP.get(apiUrl, {
+					params: {
+						'client_id': clientId,
+						'_format': format
+					}
+				},
+				function(error, data) {
+					console.log('http.get ::', error, data);
+				});
+
+			return true;
+		} catch (e) {
+			// Got a network error, time-out or HTTP error in the 400 or 500 range.
+			return false;
+		}
+	}
+});
