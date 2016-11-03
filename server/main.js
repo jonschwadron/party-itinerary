7a16b2143898eb83c4bd203c7504fef025f4c0d6
jhu <johnsonhu@gmail.com>
2016-11-02 21:57:49 -0400

    parsing the json response data into js

--- server/main.js
+++ server/main.js
@@ -17,28 +17,23 @@ if (Meteor.isServer) {
 			var clientId = '3092nxybyb0otqw18e8nh5nty';
 			var format = 'v1_legacy_for_p3';
 
-			// TODO need to sanitize the listingId
 			var apiUrl = 'https://api.airbnb.com/v2/listings/' + listingId;
 
-			try {
-				// async call
-				HTTP.get(apiUrl, {
-					headers: {
-        				'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
-					},
-					params: {
-						'client_id': clientId,
-						'_format': format
-					}
+			var result = HTTP.get(apiUrl, {
+				headers: {
+      				'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
 				},
-				function(error, data) {
-					console.log('http.get ::', error, data);
-				});
+				params: {
+					'client_id': clientId,
+					'_format': format
+				}
+			});
 
-				return true;
-			} catch (e) {
-				// Got a network error, time-out or HTTP error in the 400 or 500 range.
-				return false;
+			if(result.statusCode==200) {
+				return JSON.parse(result.content);
+			} else {
+				var errorJson = JSON.parse(result.content);
+				throw new Meteor.Error(result.statusCode, errorJson.error);
 			}
 		}
 	});
